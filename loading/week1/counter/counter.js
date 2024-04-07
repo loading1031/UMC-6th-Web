63e8df74e0d3a93709d610de5a78b035765f43d3
loading1031 <tjdans1031@gmail.com>
2024-04-07 22:48:40 +0900

    ✨Feat: 카운터 구현하기

--- loading/week1/counter/counter.js
+++ loading/week1/counter/counter.js
@@ -7,10 +7,14 @@ console.log(increase);
 console.log(decrease);
 
 increase.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  number.innerText = current + 1;
   console.log("increase 가 클릭됨");
 };
 
 decrease.onclick = () => {
+  const current = parseInt(number.innerText, 10);
+  number.innerText = current - 1;
   console.log("decrease 가 클릭됨");
 };
   
