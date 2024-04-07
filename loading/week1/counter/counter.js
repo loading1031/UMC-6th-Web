2face4206416895bdb54bbc6a6ad56c0e0ab5120
loading1031 <tjdans1031@gmail.com>
2024-04-07 22:40:58 +0900

    ✨Feat: Counter DOM 선택하기

---
+++ loading/week1/counter/counter.js
@@ -0,0 +1,8 @@
+const number = document.getElementById("number");
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+
+console.log(number);
+console.log(increase);
+console.log(decrease);
+  
