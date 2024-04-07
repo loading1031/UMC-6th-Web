5ae40567ec492232594306de951cb639a8ed5233
loading1031 <tjdans1031@gmail.com>
2024-04-07 22:44:58 +0900

    ✨Feat: 버튼 클릭 이벤트 선택하기

--- loading/week1/counter/counter.js
+++ loading/week1/counter/counter.js
@@ -5,4 +5,12 @@ const decrease = document.getElementById("decrease");
 console.log(number);
 console.log(increase);
 console.log(decrease);
+
+increase.onclick = () => {
+  console.log("increase 가 클릭됨");
+};
+
+decrease.onclick = () => {
+  console.log("decrease 가 클릭됨");
+};
   
