0888c2ebee6042184e37b78bf8b2c1c851b49181
loading1031 <tjdans1031@gmail.com>
2024-04-20 17:52:45 +0900

    ✨Feat: inifinity scroll

--- loading/week3/moviePoster/src/App.jsx
+++ loading/week3/moviePoster/src/App.jsx
@@ -1,19 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import Movie from './components/Movie';
-import { API_URL,IMAGE_BASE_URL,API_KEY,API_TOKEN } from './Config.js';
+import { API_URL, API_KEY } from './Config.js';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko-KR`;
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko-KR&page=${page}`;
     
+    console.log('Fetching movies'); // 로그 추가
     fetch(endpoint)
     .then(response => response.json()) 
-    .then(response => 
-      setMovies(response.results))
-  }, []); // 의존성 배열이 빈 배열이므로 컴포넌트 마운트 시 1회 실행
+    .then(response => {
+      console.log('Fetched data:', response); // 데이터 로드 로그
+      setMovies(prev => [...prev, ...response.results]);
+    });
+  }, [page]); // 의존성 배열이 빈 배열이므로 컴포넌트 마운트 시 1회 실행
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const windowHeight = window.innerHeight;
+      const body = document.body;
+      const html = document.documentElement;
+      const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+      const windowBottom = windowHeight + window.scrollY; // pageYOffset 대신 scrollY 사용
+      if (windowBottom >= docHeight - 1) {
+        setPage(prevPage => prevPage + 1); // 페이지 번호 증가
+      }
+    };
+  
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+  
 
   return (
     <div className='background basic'>
@@ -34,5 +55,6 @@ function App() {
 }
 
 export default App;
+
 
 
