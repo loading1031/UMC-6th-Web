8e13caac2669db1b0a2364f4b678548ce615ca25
loading1031 <tjdans1031@gmail.com>
2024-04-21 11:18:28 +0900

    🐛Bug: overview 상단 잘림

---
+++ loading/week3/moviePoster/src/components/MovieList/Movie/styles.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const Poster = styled.img`
+  width: 100%;
+  height: 70%;
+`;
+
+export const PosterContainer = styled.div`
+  position: relative;
+  overflow: auto;
+  width: 200px;
+  margin: 15px;
+  background-color: #373B6A;
+
+  &:hover .movie-overview {
+    display: block;
+  }
+`;
+
+export const ContentText = styled.div`
+  margin-top: 10px;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+export const Title = styled.span`
+  margin-left: 10px;
+  color: white;
+`;
+
+export const VoteAverage = styled.span`
+  margin-right: 10px;
+  text-align: right;
+  color: white;
+`;
+
+export const MovieOverview = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0,0,0,0.7);
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  box-sizing: border-box;
+`;
