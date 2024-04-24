2a7cb3c3b237d6c92abfad5ffdca64ce71a58093
loading1031 <tjdans1031@gmail.com>
2024-04-24 20:28:36 +0900

    ♻️Refactor: set up eslint and apply

--- loading/week3/moviePoster/src/components/MovieList/styles.js
+++ loading/week3/moviePoster/src/components/MovieList/styles.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from "styled-components";
+import styled, { keyframes } from 'styled-components';
 
 export const BackgroundBasic = styled.div`
   background-color: #22264c;
