2d9f1396585ce329dbb59b76b80b2c70a5650522
loading1031 <tjdans1031@gmail.com>
2024-04-29 04:16:59 +0900

    ✨Feat: 404페이지에는 NavBar와 Footer가 안보이게 처리

--- loading/week3/moviePoster/src/App.jsx
+++ loading/week3/moviePoster/src/App.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const location = useLocation();
+  const showComponents = location.pathname !== "/404"; // NotFound 페이지가 아닌 경우
   return (
     <div>
-      <NavBar />
-      <Outlet />
-      <Footer />
+      {showComponents && <NavBar />}
+      {/* 이곳에 다른 컴포넌트들도 조건부 렌더링 할 수 있습니다 */}
+      <Outlet />  
+      {showComponents && <Footer />}
     </div>
   );
 }
