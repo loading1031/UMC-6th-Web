139c89498a976412383e439ca57bcebb10dcb7b7
loading1031 <tjdans1031@gmail.com>
2024-04-29 04:14:16 +0900

    ✨Feat: create NotFound page

---
+++ loading/week3/moviePoster/src/Pages/NotFound/index.jsx
@@ -0,0 +1,25 @@
+// NotFound.js
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Background } from "./styles";
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  // 컴포넌트 마운트 시 /404로 리다이렉트
+  React.useEffect(() => {
+    navigate("/404");
+  }, [navigate]);
+
+  return (
+    <Background>
+      <h1>404 Error</h1>
+      <p>Page not found.</p>
+      <h2>
+        <Link to="/">메인으로 이동하기</Link>
+      </h2>
+    </Background>
+  );
+};
+
+export default NotFound;
---
+++ loading/week3/moviePoster/src/Pages/NotFound/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  background-color: white;
+  width: 100%;
+  height: 100%;
+`;
