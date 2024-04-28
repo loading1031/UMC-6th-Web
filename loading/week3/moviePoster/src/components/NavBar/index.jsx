b4a4fe50b7b51ccc020eb888d48be7a4fa46b42a
loading1031 <tjdans1031@gmail.com>
2024-04-28 20:26:05 +0900

    ✨Feat: Clicking the button toggles the text between 'Login' and 'Logout'

--- loading/week3/moviePoster/src/components/NavBar/index.jsx
+++ loading/week3/moviePoster/src/components/NavBar/index.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { StyledNav, StyledUl, StyledLink } from './styles';
 
-const NavBar = () => (
-  <StyledNav>
-    <h4 style={{ margin: 0, color: 'white' }}>UMC Movie</h4>
-    <StyledUl>
-      <li><StyledLink to="/join">회원가입</StyledLink></li>
-      <li><StyledLink to="/popular">Popular</StyledLink></li>
-      <li><StyledLink to="/nowplaying">NowPlaying</StyledLink></li>
-      <li><StyledLink to="/toprated">Top Rated</StyledLink></li>
-      <li><StyledLink to="/upcoming">Upcoming</StyledLink></li>
-    </StyledUl>
-  </StyledNav>
-);
+const NavBar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // 클릭 이벤트 핸들러
+  const handleLoginClick = () => {
+    setIsLoggedIn(isLoggedIn => !isLoggedIn); // 현재 상태를 반전시킴
+  };
+
+  return(
+    <StyledNav>
+      <h4 style={{ margin: 0, color: 'white' }}>UMC Movie</h4>
+      <StyledUl>
+        <li><StyledLink to="/" onClick={handleLoginClick}>
+        {isLoggedIn ? '로그아웃' : '로그인'}
+        </StyledLink></li>
+        <li><StyledLink to="/popular">Popular</StyledLink></li>
+        <li><StyledLink to="/nowplaying">NowPlaying</StyledLink></li>
+        <li><StyledLink to="/toprated">Top Rated</StyledLink></li>
+        <li><StyledLink to="/upcoming">Upcoming</StyledLink></li>
+      </StyledUl>
+    </StyledNav>
+  );
+};
 
 export default NavBar;
