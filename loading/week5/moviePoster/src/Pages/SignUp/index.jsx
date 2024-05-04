5b24c2914044981f70623e0a094f79bd82872206
loading1031 <tjdans1031@gmail.com>
2024-05-04 20:12:52 +0900

    ✨Feat: create validate check in signUp

--- loading/week5/moviePoster/src/Pages/SignUp/index.jsx
+++ loading/week5/moviePoster/src/Pages/SignUp/index.jsx
@@ -7,8 +7,9 @@ import {
   WhiteP,
   HorizonDiv,
   FormContainer,
-  WarningP
+  WarningP,
 } from "./styles";
+import { validateField, validateForm } from "./validate";
 
 function SignUp() {
   const [formData, setFormData] = useState({
@@ -27,38 +28,42 @@ function SignUp() {
     check_pw: "",
   });
 
+  // 모든 에러가 비어있는지 검사하여 폼의 유효성을 판단
+  const isFormValid = () => {
+    return Object.values(errors).every((error) => error === "");
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault(); // 폼의 기본 제출 동작 방지
-    let valid = true;
-    const newErrors = {};
+    const { isValid, errors } = validateForm(formData);
+    setErrors(errors);
 
-    // 필드별 공백 검사
-    Object.keys(formData).forEach(key => {
-      if (!formData[key].trim()) {
-        valid = false;
-        newErrors[key] = "이 필드는 필수입니다.";
-      } else {
-        newErrors[key] = "";
-      }
-    });
-
-    setErrors(newErrors);
-
-    if (valid) {
-      console.log('Submitted with Email:', formData.email, 'Password:', formData.password);
+    if (isValid) {
+      console.log(
+        "Submitted with Email:",
+        formData.email,
+        "Password:",
+        formData.password
+      );
       // 폼 제출 로직
     }
   };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-    setFormData(prevFormData => ({
+    setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
     }));
-    setErrors(prevErrors => ({
+    // setFormData와 비동기적으로 처리될 수 있기 때문에, formData의 최신 상태를 불러옴(setFormData와는 무관함)
+    const { valid, error } = validateField(name, value, {
+      ...formData,
+      [name]: value,
+    });
+
+    setErrors((prevErrors) => ({
       ...prevErrors,
-      [name]: value ? "" : "이 필드는 필수입니다." // 입력 시 에러 메시지 초기화
+      [name]: valid ? "" : error, // 유효하면 오류 메시지를 비움, 아니면 오류 메시지 설정
     }));
   };
 
@@ -79,6 +84,7 @@ function SignUp() {
           type="email"
           value={formData.email}
           placeholder="이메일을 입력해주세요"
+          autoComplete="username" // email 자동완성
           onChange={handleChange}
         />
         {errors.email && <WarningP>{errors.email}</WarningP>}
@@ -87,6 +93,8 @@ function SignUp() {
           type="number"
           value={formData.age}
           placeholder="나이를 입력해주세요"
+          // min="19"
+          // step="1"
           onChange={handleChange}
         />
         {errors.age && <WarningP>{errors.age}</WarningP>}
@@ -95,6 +103,7 @@ function SignUp() {
           type="password"
           value={formData.password}
           placeholder="비밀번호를 입력해주세요"
+          autoComplete="new-password" // 비밀번호 자동완성 차단
           onChange={handleChange}
         />
         {errors.password && <WarningP>{errors.password}</WarningP>}
@@ -103,19 +112,21 @@ function SignUp() {
           type="password"
           value={formData.check_pw}
           placeholder="비밀번호 확인"
+          autoComplete="new-password"
           onChange={handleChange}
         />
         {errors.check_pw && <WarningP>{errors.check_pw}</WarningP>}
       </StyledFieldset>
-      
-      <StyledButton>제출하기</StyledButton>
+
+      <StyledButton disabled={!isFormValid()}>제출하기</StyledButton>
       <HorizonDiv>
         <WhiteP>이미 아이디가 있으신가요?</WhiteP>
-        <WhiteP><strong>로그인 페이지로 이동하기</strong></WhiteP>
+        <WhiteP>
+          <strong>로그인 페이지로 이동하기</strong>
+        </WhiteP>
       </HorizonDiv>
     </FormContainer>
   );
 }
 
 export default SignUp;
-
