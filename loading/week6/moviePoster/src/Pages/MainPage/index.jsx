b009753c5c3bc1b184abab326fa0ee8314b1b1d9
loading1031 <tjdans1031@gmail.com>
2024-05-15 19:50:09 +0900

    🔨Fix: Don't show loading text when input is null

--- loading/week6/moviePoster/src/Pages/MainPage/index.jsx
+++ loading/week6/moviePoster/src/Pages/MainPage/index.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   BottomContainer,
   CenteredContainer,
@@ -9,11 +9,11 @@ import {
   StyledButton,
   StyledH1,
   StyledInput,
-} from './styles';
-import { Search } from './Search';
+} from "./styles";
+import { Search } from "./Search";
 
 function MainPage() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [search, setSearch] = useState(false);
   const [movies, setMovies] = useState([]);
 
@@ -21,7 +21,7 @@ function MainPage() {
     const timeoutId = setTimeout(async () => {
       if (!input.trim()) {
         setSearch(false);
-        return;  // 입력 값이 없는 경우 요청을 보내지 않습니다.
+        return; // 입력 값이 없는 경우 요청을 보내지 않습니다.
       }
 
       const endpoint = `${import.meta.env.VITE_API_URL}search/movie?api_key=${
@@ -32,16 +32,15 @@ function MainPage() {
         const response = await axios.get(endpoint);
         console.log(response.data);
         setMovies(response.data.results);
-        setSearch(response.data.results.length>0);
+        setSearch(response.data.results.length > 0);
       } catch (error) {
         console.error("요청을 처리하는 중에 오류가 발생했습니다:", error);
         setSearch(false);
       }
-    }, 500);  // 500ms 후에 실행
+    }, 500); // 500ms 후에 실행
 
-    return () => clearTimeout(timeoutId);  // 컴포넌트 언마운트시 또는 input 변경시 타이머 취소
-  }, [input]);  // input 상태가 변할 때마다 이 effect가 실행됩니다.
-
+    return () => clearTimeout(timeoutId); // 컴포넌트 언마운트시 또는 input 변경시 타이머 취소
+  }, [input]); // input 상태가 변할 때마다 이 effect가 실행됩니다.
 
   return (
     <div>
@@ -49,7 +48,7 @@ function MainPage() {
         <StyledH1>환영합니다</StyledH1>
       </CenteredContainer>
       <BottomContainer>
-        <StyledH1 style={{ fontSize: '48px' }}>📽️ Find your movies!</StyledH1>
+        <StyledH1 style={{ fontSize: "48px" }}>📽️ Find your movies!</StyledH1>
         <FormContainer onSubmit={(e) => e.preventDefault()}>
           <InputButtonContainer>
             <StyledInput
@@ -62,7 +61,11 @@ function MainPage() {
             </StyledButton>
           </InputButtonContainer>
         </FormContainer>
-        {search ? (<Search movies={movies} search={search}/>):(<LoadingTxt>데이터를 받아오는 중입니다.</LoadingTxt>)}
+        {search ? (
+          <Search movies={movies} search={search} />
+        ) : (
+          input && <LoadingTxt>데이터를 받아오는 중입니다.</LoadingTxt>
+        )}
       </BottomContainer>
     </div>
   );
